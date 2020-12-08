15b9d7e5fedf25f6876d54f66c42a560cdeeb897
MarcoPolokk <58775058+MarcoPolokk@users.noreply.github.com>
2020-12-08 19:59:14 +0100

    Accumulating digits on display - done

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -11,10 +11,22 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var displayLabel: UILabel!
     
+    var isFinishedTypingNumber: Bool = true
+    
     @IBAction func calcButtonPressed(_ sender: UIButton) {
     }
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
+        
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber == true {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text = displayLabel.text! + numValue
+            }
+            
+        }
     }
     
     
